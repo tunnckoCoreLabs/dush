484f3a03f319f87bea4c2a4bb1e145912a2bae26
tunnckoCore <mameto_100@mail.bg>
2017-03-12 19:25:48 +0200

    fix(support): make it work on 0.10 and 0.12

--- test.js
+++ test.js
@@ -9,11 +9,11 @@
 
 'use strict'
 
-const test = require('mukla')
-const dush = require('./dist/dush.common')
-const app = dush()
+var test = require('mukla')
+var dush = require('./dist/dush.common')
+var app = dush()
 
-test('should return an instance with methods and `.all` object', (done) => {
+test('should return an instance with methods and `.all` object', function (done) {
   test.strictEqual(typeof app.all, 'object')
   test.strictEqual(typeof app.use, 'function')
   test.strictEqual(typeof app.on, 'function')
@@ -24,7 +24,7 @@ test('should return an instance with methods and `.all` object', (done) => {
 })
 
 test('should instace has .all object that contains all handlers', function (done) {
-  const fn = () => {}
+  var fn = function () {}
 
   app.on('aaa', fn)
   app.on('aaa', fn)
@@ -42,17 +42,17 @@ test('should instace has .all object that contains all handlers', function (done
 })
 
 test('should register handlers for any type of string', function (done) {
-  const app = dush()
-  app.on('constructor', (a) => {
+  var app = dush()
+  app.on('varructor', function (a) {
     test.ok(a === 2)
   })
-  app.emit('constructor', 2)
+  app.emit('varructor', 2)
   done()
 })
 
 test('should .emit with multiple params (maximum 3)', function (done) {
-  const emitter = dush()
-  emitter.on('foo', (a, b) => {
+  var emitter = dush()
+  emitter.on('foo', function (a, b) {
     test.strictEqual(a, 'aaa')
     test.strictEqual(b, 'bbb')
   })
@@ -62,8 +62,8 @@ test('should .emit with multiple params (maximum 3)', function (done) {
 })
 
 test('should handler should not get 4th param', function (done) {
-  const app = dush()
-  app.once('quxie', (a, b, c, d) => {
+  var app = dush()
+  app.once('quxie', function (a, b, c, d) {
     test.strictEqual(a, 1)
     test.strictEqual(b, 2)
     test.strictEqual(c, 3)
@@ -74,8 +74,8 @@ test('should handler should not get 4th param', function (done) {
 })
 
 test('should .on register multiple handlers', function (done) {
-  let called = 0
-  let fn = (a) => {
+  var called = 0
+  var fn = function (a) {
     called++
     test.strictEqual(a, 123)
   }
@@ -89,8 +89,8 @@ test('should .on register multiple handlers', function (done) {
 })
 
 test('should handlers added with .once be called one time only', function (done) {
-  let called = 0
-  app.once('bar', () => {
+  var called = 0
+  app.once('bar', function () {
     called++
   })
 
@@ -103,14 +103,14 @@ test('should handlers added with .once be called one time only', function (done)
 })
 
 test('should .off("foo", fn) remove the handler', function (done) {
-  let called = 0
-  let second = 0
-  const fn = () => {
+  var called = 0
+  var second = 0
+  var fn = function () {
     called++
   }
 
   app.on('qux', fn)
-  app.on('qux', () => {
+  app.on('qux', function () {
     second = 1
   })
   app.off('qux', fn)
@@ -124,8 +124,8 @@ test('should .off("foo", fn) remove the handler', function (done) {
 
 test('should .off("foo") remove all "foo" handlers', function (done) {
   app
-    .on('zzz', () => {})
-    .on('zzz', () => {})
+    .on('zzz', function () {})
+    .on('zzz', function () {})
     .off('zzz')
 
   test.strictEqual(app.all.zzz.length, 0)
@@ -133,16 +133,16 @@ test('should .off("foo") remove all "foo" handlers', function (done) {
 })
 
 test('should all methods be chainable', function (done) {
-  let called = 0
-  const foo = app.on('foo', () => {})
+  var called = 0
+  var foo = app.on('foo', function () {})
   test.ok(foo.once)
 
-  const bar = foo.once('bar', () => {
+  var bar = foo.once('bar', function () {
     called++
   })
   test.ok(bar.emit)
 
-  const qux = bar.emit('bar')
+  var qux = bar.emit('bar')
   test.ok(qux.off)
 
   qux.off('bar').emit('bar')
@@ -151,8 +151,8 @@ test('should all methods be chainable', function (done) {
 })
 
 test('should have wildcard event', function (done) {
-  const app = dush()
-  app.on('*', (name, nume) => {
+  var app = dush()
+  app.on('*', function (name, nume) {
     test.strictEqual(name, 'haha')
     test.strictEqual(nume, 444444)
   })
@@ -161,16 +161,16 @@ test('should have wildcard event', function (done) {
 })
 
 test('should return app if .use(plugin) dont', function (done) {
-  const app = dush()
+  var app = dush()
   app
-    .use((app) => {
+    .use(function (app) {
       app.foo = 'bar'
     })
-    .use((app) => {
+    .use(function (app) {
       app.baz = 12345
       return app
     })
-    .use((app) => {
+    .use(function (app) {
       app.qux = 'zzz'
     })
 
